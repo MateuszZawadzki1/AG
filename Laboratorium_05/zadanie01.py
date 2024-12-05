8084b09cfbe00d3931133ffeed60459b33a1ac4b
Mateusz Zawadzki <74826603+MateuszZawadzki1@users.noreply.github.com>
2024-12-05 12:55:38 +0100

    Add files via upload

--- Laboratorium_05/zadanie01.py
+++ Laboratorium_05/zadanie01.py
@@ -22,6 +22,9 @@ class Graph:
                 self.graph[v].pop(index)
  
 
+    # ___________________________________________________
+
+
     def DFSCount(self, v, visited):
         count = 1
         visited[v] = True
@@ -33,38 +36,25 @@ class Graph:
     
 
     def isValidNextEdge(self, u, v):
-  
-  
-      
         if len(self.graph[u]) == 1:
             return True
         else:
-            '''
-             2) If there are multiple adjacents, then u-v is not a bridge
-                 Do following steps to check if u-v is a bridge
-  
-            2.a) count of vertices reachable from u'''   
+
             visited =[False]*(self.V)
             count1 = self.DFSCount(u, visited)
- 
-            '''2.b) Remove edge (u, v) and after removing the edge, count
-                vertices reachable from u'''
             self.rmvEdge(u, v)
             visited =[False]*(self.V)
             count2 = self.DFSCount(u, visited)
  
-            #2.c) Add the edge back to the graph
+
             self.addEdge(u,v)
  
-            # 2.d) If count1 is greater, then edge (u, v) is a bridge
             return False if count1 > count2 else True
  
  
-    # Print Euler tour starting from vertex u
+
     def printEulerUtil(self, u):
-        #Recur for all the vertices adjacent to this vertex
         for v in self.graph[u]:
-            #If edge u-v is not removed and it's a a valid next edge
             if self.isValidNextEdge(u, v):
                 print("%d-%d " %(u,v)),
                 self.rmvEdge(u, v)
@@ -72,22 +62,18 @@ class Graph:
  
  
      
-    '''The main function that print Eulerian Trail. It first finds an odd
-   degree vertex (if there is any) and then calls printEulerUtil()
-   to print the path '''
+
     def printEulerTour(self):
-        #Find a vertex with odd degree
         u = 0
         for i in range(self.V):
             if len(self.graph[i]) %2 != 0 :
                 u = i
                 break
-        # Print tour starting from odd vertex
         print ("\n")
         self.printEulerUtil(u)
  
-# Create a graph given in the above diagram
  
+
 g1 = Graph(4)
 g1.addEdge(0, 1)
 g1.addEdge(0, 2)
@@ -95,13 +81,14 @@ g1.addEdge(1, 2)
 g1.addEdge(2, 3)
 g1.printEulerTour()
  
- 
+
 g2 = Graph(3)
 g2.addEdge(0, 1)
 g2.addEdge(1, 2)
 g2.addEdge(2, 0)
 g2.printEulerTour()
  
+
 g3 = Graph (5)
 g3.addEdge(1, 0)
 g3.addEdge(0, 2)
@@ -114,4 +101,4 @@ g3.addEdge(2, 4)
 g3.printEulerTour()
  
  
-#This code is contributed by Neelam Yadav
+
